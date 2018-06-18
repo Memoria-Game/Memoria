2410510de2c3a95f126d142d8cfe50207903d3f2
francoisquellec <francois.quellec@heig-vd.ch>
2018-06-18 19:20:41 +0200

    fix

--- js/stats.js
+++ js/stats.js
@@ -36,6 +36,45 @@ function addFriend(username){
   })
 }
 
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("friends_table");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = parseInt(rows[i].getElementsByTagName("TD")[2].innerHTML);
+      y = parseInt(rows[i + 1].getElementsByTagName("TD")[2].innerHTML);
+      console.log(x);
+      console.log(y);
+      // Check if the two rows should switch place:
+      if (x < y) {
+        // If so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+  alert("test"); 
+}
+
 function friends_data(){
     getFriendScores().then((data) =>{
         let rows = $("#friends_table").find("tbody")
@@ -52,50 +91,11 @@ function friends_data(){
             rows.append(elem)
             id += 1
         }
-
+        sortTable();
     } )
 }
 
 friends_data();
-function sortTable() {
-              var table, rows, switching, i, x, y, shouldSwitch;
-              table = document.getElementById("friends_table");
-              switching = true;
-              /* Make a loop that will continue until
-              no switching has been done: */
-              while (switching) {
-                // Start by saying: no switching is done:
-                switching = false;
-                rows = table.getElementsByTagName("TR");
-                /* Loop through all table rows (except the
-                first, which contains table headers): */
-                for (i = 1; i < (rows.length - 1); i++) {
-                  // Start by saying there should be no switching:
-                  shouldSwitch = false;
-                  /* Get the two elements you want to compare,
-                  one from current row and one from the next: */
-                  x = parseInt(rows[i].getElementsByTagName("TD")[1].innerHTML);
-                  y = parseInt(rows[i + 1].getElementsByTagName("TD")[1].innerHTML);
-                  console.log(x);
-                  console.log(y);
-                  // Check if the two rows should switch place:
-                  if (x > y) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                  }
-                }
-                if (shouldSwitch) {
-                  /* If a switch has been marked, make the switch
-                  and mark that a switch has been done: */
-                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                  switching = true;
-                }
-              }
-                alert("Sorted");
-            }
-            sortTable();
-
 
 function add_friend_data(){
     let name = $('#friend_name').val()
